4b7a65d4cb83936d6576c3a9c85d16e08ddf5107
Stefan Junker <mail@stefanjunker.de>
2023-04-28 12:36:57 +0200

    wip

--- flake.nix
+++ flake.nix
@@ -1,11 +1,20 @@
 {
   inputs =
     {
+      holochain.url = "github:holochain/holochain/holochain-0.2.0";
+      holochain.flake = false;
+      lair.url = "github:holochain/lair/lair_keystore-v0.2.4";
+      lair.flake = false;
+      launcher.url = "github:holochain/launcher/holochain-0.2";
+      launcher.flake = false;
+      scaffolding.url = "github:holochain/scaffolding/holochain-0.2";
+      scaffolding.flake = false;
+
       holochain-flake.url = "github:holochain/holochain";
-      holochain-flake.inputs.holochain.url = "github:holochain/holochain/holochain-0.2.0";
-      holochain-flake.inputs.lair.url = "github:holochain/lair/lair_keystore-v0.2.4";
-      holochain-flake.inputs.launcher.url = "github:holochain/launcher/holochain-0.2";
-      holochain-flake.inputs.scaffolding.url = "github:holochain/scaffolding/holochain-0.2";
+      holochain-flake.inputs.holochain.follows = "holochain";
+      holochain-flake.inputs.lair.follows = "lair";
+      holochain-flake.inputs.launcher.follows = "launcher";
+      holochain-flake.inputs.scaffolding.follows = "scaffolding";
 
       nixpkgs.follows = "holochain-flake/nixpkgs";
       flake-parts.follows = "holochain-flake/flake-parts";
