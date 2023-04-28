b75af79df141fea4fa062468a7187ae3f8e102f1
Stefan Junker <mail@stefanjunker.de>
2023-04-28 12:13:20 +0200

    wip

--- flake.nix
+++ flake.nix
@@ -1,28 +1,14 @@
 {
   inputs =
     {
-      holochain = {
-        url = "github:holochain/holochain/holochain-0.2.0";
-        flake = false;
-      };
+      holochain-flake.url = "github:holochain/holochain";
+      holochain-flake.inputs.holochain.url = "github:holochain/holochain/holochain-0.2.0";
+      holochain-flake.inputs.lair.url = "github:holochain/lair/lair_keystore-v0.2.4";
+      holochain-flake.inputs.launcher.url = "github:holochain/launcher/holochain-0.2";
+      holochain-flake.inputs.scaffolding.url = "github:holochain/scaffolding/holochain-0.2";
 
-      lair = {
-        url = "github:holochain/lair/lair_keystore-v0.2.4";
-        flake = false;
-      };
-
-      # holochain_cli_launch
-      launcher = {
-        url = "github:holochain/launcher/holochain-0.2";
-        flake = false;
-      };
-
-      # holochain_scaffolding_cli
-      scaffolding = {
-        url = "github:holochain/scaffolding/holochain-0.2";
-        flake = false;
-      };
+      nixpkgs.follows = "holochain-flake/nixpkgs";
     };
 
-  outputs = { ... }: { };
+  outputs = inputs: inputs.holochain-flake.outputs;
 }
