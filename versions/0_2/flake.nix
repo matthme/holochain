899d3c54125dd2dd600e0be0a288340db06458b5
Stefan Junker <mail@stefanjunker.de>
2023-04-28 14:11:46 +0200

    wip

--- versions/0_2/flake.nix
+++ versions/0_2/flake.nix
@@ -1,7 +1,7 @@
 {
   inputs =
     {
-      holochain.url = "github:holochain/holochain/main-0.2";
+      holochain.url = "github:holochain/holochain/holochain-0.2.0-beta-rc.7";
       holochain.flake = false;
       lair.url = "github:holochain/lair/lair_keystore-v0.2.4";
       lair.flake = false;
