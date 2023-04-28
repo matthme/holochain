92eaeb7fae391779d9ceb50a8ce4106f2b682255
Stefan Junker <mail@stefanjunker.de>
2023-04-28 12:03:14 +0200

    wip

--- flake.nix
+++ flake.nix
@@ -3,8 +3,9 @@
     "Holochain is an open-source framework to develop peer-to-peer applications with high levels of security, reliability, and performance.";
 
   inputs = {
-    "versions_0_1".url = "path:./versions/0_1";
-    "versions_0_2".url = "path:./versions/0_2";
+    versions_0_1.url = "path:./versions/0_1";
+    versions_0_2.url = "path:./versions/0_2";
+    versions.url = "path:./versions/0_2";
   };
 
   # refer to flake-parts docs https://flake.parts/
