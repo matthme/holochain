f139d1c30d803a04dd94f1d6ebd6d3bf19d7be16
Stefan Junker <mail@stefanjunker.de>
2023-05-03 18:29:13 +0200

    pin new lair

--- holonix/examples/custom_versions/flake.nix
+++ holonix/examples/custom_versions/flake.nix
@@ -11,12 +11,12 @@
     holochain-versions.url = "github:holochain/holochain/experiment_flake_lock_mangling?dir=versions/0_1";
 
     holochain-flake.url = "github:holochain/holochain/experiment_flake_lock_mangling";
-    holochain-flake.inputs.versions.url = "github:holochain/holochain/experiment_flake_lock_mangling?dir=versions/0_1";
+    # holochain-flake.inputs.versions.url = "github:holochain/holochain/experiment_flake_lock_mangling?dir=versions/0_1";
 
-    # holochain-flake.inputs.holochain.follows = "holochain-versions/holochain";
-    # holochain-flake.inputs.lair.follows = "holochain-versions/lair";
-    # holochain-flake.inputs.launcher.follows = "holochain-versions/launcher";
-    # holochain-flake.inputs.scaffolding.follows = "holochain-versions/scaffolding";
+    holochain-flake.inputs.holochain.follows = "holochain-versions/holochain";
+    holochain-flake.inputs.lair.follows = "holochain-versions/lair";
+    holochain-flake.inputs.launcher.follows = "holochain-versions/launcher";
+    holochain-flake.inputs.scaffolding.follows = "holochain-versions/scaffolding";
 
     nixpkgs.follows = "holochain-flake/nixpkgs";
     flake-parts.follows = "holochain-flake/flake-parts";
